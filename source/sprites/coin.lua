be9adabe7dc9c35f7fd2e778e98efaf6a4ff2ae9
Maxime <maxrofra@gmail.com>
2023-11-30 13:05:01 +0100

    coin config

--- source/sprites/coin.lua
+++ source/sprites/coin.lua
@@ -16,37 +16,31 @@ function Coin:init()
 	self:setCenter(0, 0)
 	self:setCollideRect(self:getBounds())
 	
-	self.hasBeenGrabbed = false
+	self.config = {
+		isPicked = false
+	}
 end
 
 function Coin:loadConfig(config)
 	Coin.super.loadConfig(self, config)
 	
-	if config.isPicked == nil then
-		config.isPicked = false
-		
-		print("WARNING: [Coin:loadConfig] - no config value for 'isPicked'! Setting default value...")
-	end
+	self.config = table.shallowcopy(config)
 	
-	self.hasBeenGrabbed = config.isPicked
-	self:updateVisible(not self.hasBeenGrabbed)
+	self:setVisible(not self.config.isPicked)
 end
 
 function Coin:updateConfig(config)
-	config.isPicked = self.hasBeenGrabbed
+	config.isPicked = self.config.isPicked
 end
 
 function Coin:reset()
-	self.hasBeenGrabbed = false
+	self.config.isPicked = false
 	
-	self:updateVisible(not self.hasBeenGrabbed)
+	self:setVisible(not self.config.isPicked)
 end
 
 function Coin:isGrabbed()
-	self.hasBeenGrabbed = true
-	self:updateVisible(not self.hasBeenGrabbed)
-end
-
-function Coin:updateVisible(isVisible)
-	self:setVisible(isVisible)
+	self.config.isPicked = true
+	
+	self:setVisible(not self.config.isPicked)
 end
