a2fe506795e81c74de68a464d8358a0797c4d141
Maxime <maxrofra@gmail.com>
2023-12-11 15:52:47 +0100

    Background draw rect handling using overlap

--- source/widgets/play/background.lua
+++ source/widgets/play/background.lua
@@ -38,24 +38,27 @@ function WidgetBackground:_draw(frame, rect)
 		local imageOffset = math.floor(self.imageOffsets[i])
 		local imageWidth, imageHeight = image:getSize()
 		
-		-- TODO: Handle drawRect / image visible rects overlap
 		local imageRightRect = Rect.make(imageOffset, 0, imageWidth, imageHeight)
 		local imageLeftRect = Rect.make(imageOffset - 400, 0, imageWidth, imageHeight)
 		local screenRect = Rect.make(0, 0, 400, 240)
 		
 		-- Draw 2 copies of the image, one before and one after
-		local imageRightSourceRect = Rect.offset(Rect.overlap(imageRightRect, screenRect), -imageOffset, 0)
-		assert(imageRightSourceRect.x == 0)
-		assert(imageRightSourceRect.y == 0)
-		assert(imageRightSourceRect.w == imageWidth - math.abs(imageOffset))
-		assert(imageRightSourceRect.h == 240)
+		local imageRightOverlapRect
+		if rect == nil then
+			imageRightOverlapRect = Rect.overlap(imageRightRect, screenRect)
+		else
+			imageRightOverlapRect = Rect.overlap(Rect.overlap(imageRightRect, screenRect), rect)
+		end
+		local imageRightSourceRect = Rect.offset(imageRightOverlapRect, -imageOffset, 0)
 		image:draw(frame.x + imageOffset, frame.y, playdate.graphics.kImageUnflipped, imageRightSourceRect.x, imageRightSourceRect.y, imageRightSourceRect.w, imageRightSourceRect.h)
 		
-		local imageLeftSourceRect = Rect.offset(Rect.overlap(imageLeftRect, screenRect), imageWidth - imageOffset, 0)
-		assert(imageLeftSourceRect.x == imageWidth - imageOffset)
-		assert(imageLeftSourceRect.y == 0)
-		assert(imageLeftSourceRect.w == imageOffset)
-		assert(imageLeftSourceRect.h == 240)
+		local imageLeftOverlapRect
+		if rect == nil then
+			imageLeftOverlapRect = Rect.overlap(imageLeftRect, screenRect)
+		else
+			imageLeftOverlapRect = Rect.overlap(Rect.overlap(imageLeftRect, screenRect), rect)
+		end
+		local imageLeftSourceRect = Rect.offset(imageLeftOverlapRect, imageWidth - imageOffset, 0)
 		image:draw(frame.x + imageOffset - imageOffset, frame.y, playdate.graphics.kImageUnflipped, imageLeftSourceRect.x, imageLeftSourceRect.y, imageLeftSourceRect.w, imageLeftSourceRect.h)
 	end
 end
