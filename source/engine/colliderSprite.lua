cfc4a4663a3c383eabd2bb87a535cf4f9a86d8ef
Matthis <matthishoc@gmail.com>
2024-01-26 17:12:20 +0100

    collision test functions

--- source/engine/colliderSprite.lua
+++ source/engine/colliderSprite.lua
@@ -50,3 +50,39 @@ end
 function ColliderSprite:getCollisionType()
     return self._collisionType
 end
+
+-- Returns true if a rect and a circle intersect
+function ColliderSprite:_aabbToCircle(rect, circle)
+    local rectMin = playdate.geometry.point.new(rect.x, rect.y)
+    local rectMax = playdate.geometry.point.new(rect.x + rect.width, rect.y + rect.height)
+
+    -- Here we clamp the circle position to be in bound of the rectangle tested against
+    local circleCenter = playdate.geometry.point.new(circle.x, circle.y)
+    local closestPoint = playdate.geometry.point.copy(circleCenter)
+    if closestPoint.x < rectMin.x then
+        closestPoint.x = rectMin.x
+    elseif closestPoint.x > rectMax.x then
+        closestPoint.x = rectMax.x
+    end
+
+    if closestPoint.y < rectMin.y then
+        closestPoint.y = rectMin.y
+    elseif closestPoint.y > rectMax.y then
+        closestPoint.y = rectMax.y
+    end
+
+    return closestPoint.squaredDistanceToPoint(circleCenter) <= circle.radius * circle.radius
+end
+
+function ColliderSprite:_circleToCircle(a, b)
+    local aPos = playdate.geometry.point.new(a.x, a.y)
+    local bPos = playdate.geometry.point.new(b.x, b.y)
+
+    local radiiSum = a.radius + b.radius
+    return aPos.squaredDistanceToPoint(bPos) <= radiiSum * radiiSum
+end
+
+function ColliderSprite:_aabbToAabb(a, b)
+    local x, y, w, h = playdate.geometry.rect.fast_intersection(a.x, a.y, a.width, a.height, b.x, b.y, b.width, b.height)
+    return x == 0 and y == 0 and w == 0 and h == 0
+end
